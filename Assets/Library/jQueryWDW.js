a5488d660fe9d5528957f96ce23a38dcdb63aac3
SCN <supercodingninja@outlook.com>
2020-11-16 02:08:16 -0800

    Fetch API

--- Assets/Library/jQueryWDW.js
+++ Assets/Library/jQueryWDW.js
@@ -1,14 +1,13 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://weather-com.p.rapidapi.com/v3/wx/forecast/daily/undefined?units=undefined&geocode=34.080911%2C-118.270406&language=en",
+fetch("https://weather-com.p.rapidapi.com/v3/wx/forecast/daily/undefined?units=undefined&geocode=34.080911%2C-118.270406&language=en", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "87fe806293mshd2b0a915f2b154ap1f05e8jsnd65e7e646933",
 		"x-rapidapi-host": "weather-com.p.rapidapi.com"
 	}
-};
-
-$.ajax(settings).done(function (response) {
+})
+.then(response => {
 	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
